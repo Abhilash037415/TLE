c999fa3c11c62a628663164de86ebc020b420888
Abhilash Munnangi <abhilashmunnangi12@gmail.com>
2026-05-17 02:41:34 +0530

    Add Flask server for keep-alive functionality

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "TLE Bot Running"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
